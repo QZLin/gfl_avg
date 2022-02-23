81340a4cf2c86e9696f44f4d7eaabdff7f6a17ab
QZLin <qzlin01@163.com>
2022-02-23 19:07:17 +0800

    optimize ps script

--- clean.ps1
+++ clean.ps1
@@ -2,11 +2,9 @@ param(
     [string]$target_path = "."
 )
 
-$current_path = Get-Location
-
-Set-Location $target_path
+Push-Location $target_path
 Write-Output "Clean '*.rpyc','*.rpyc.bak' for $target_path"
 Remove-Item ./*.rpyc
 Remove-Item ./*.rpyc.bak
 
-Set-Location $current_path
+Pop-Location
