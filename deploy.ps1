a1ce8255580e022bb07b2e8e052eae80eadede2a
QZLin <qzlin01@163.com>
2022-02-28 09:19:20 +0800

    optimize avg bg handle python script args

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,19 @@
-# Recommend Powershell-7
+#Requires -Version 7.0
+function config($name) {
+    return python -c "import tool.config;print(tool.config.$name)"
+}
+
 Push-Location $PSScriptRoot
-$renpy_prj = Get-Content "tool/config.py" | Select-String -Pattern "RENPY_PROJECT\s*=\s*r?'(.*)'" | ForEach-Object { $_.Matches.Groups[1].value }
-$renpy_prj = $renpy_prj -replace "\\\\", "\"
+#$renpy_prj = Get-Content "tool/config.py" | Select-String -Pattern "RENPY_PROJECT\s*=\s*r?'(.*)'" | ForEach-Object { $_.Matches.Groups[1].value }
+#$renpy_prj = $renpy_prj -replace "\\\\", "\"
+$renpy_prj = config("RENPY_PROJECT")
 Write-Output "Renpy Project: $renpy_prj"
-$target = "$renpy_prj\game"
+$target = [IO.Path]::Combine($renpy_prj, "game")
+$scripts = [IO.Path]::Combine($target, "script_level")
 
-&"./clean.ps1" "$target\script_level"
+&"./clean.ps1" $scripts
 
-Copy-Item ./rpy/*.* $target/script_level -Force
-Write-Output "Copy rpy/*.* to $target\script_level, copy rpy/script.rpy to $target"
-Move-Item $target/script_level/script.rpy $target -Force
+Copy-Item ./rpy/*.* $scripts -Force
+Write-Output "Copy rpy/*.* to $scripts, copy rpy/script.rpy to $target"
+Move-Item ([IO.Path]::Combine($scripts, "script.rpy")) $target -Force
 Pop-Location
--- tool/avg_bg_crop.ps1
+++ tool/avg_bg_crop.ps1
@@ -10,12 +10,17 @@ $target_path = config("AVG_BG_TARGET")
 Write-Output "SourceDir: $image_path,`nTargetDir$target_path"
 # Pop-Location
 # Push-Location $image_path
+$sw = [Diagnostics.Stopwatch]::StartNew()
 Get-ChildItem $image_path -Filter "*.png" | ForEach-Object {
     # Start-Process python -Args "tool/avg_bg_handle.pya", $_.FullName -RedirectStandardOutput (Join-Path $target_path $_.Name) -RedirectStandardError $true -Verbose
     # [io.file]::WriteAllBytes((Join-Path $target_path $_.Name),
     # (./tool/Invoke-BinaryProcess.ps1 python.exe -Output -ArgumentList "tool/avg_bg_handle.py", $_.FullName))
     Write-Output $_.Name
-    python "tool/avg_bg_handle.py" $_.FullName -f -o (Join-Path $target_path $_.Name)
+    $st = $sw.ElapsedMilliseconds
+    python "tool/avg_bg_handle.py" $_.FullName -f (Join-Path $target_path $_.Name)
+    Write-Output "$(($sw.ElapsedMilliseconds-$st)/1000)s"
 }
+$sw.Stop()
+Write-Output "Finished,total:$($sw.ElapsedMilliseconds/1000)s"
 
 Pop-Location
