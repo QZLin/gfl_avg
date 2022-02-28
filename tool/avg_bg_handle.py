331277546a46f1eae0f12f5b5bef5b1665aee10b
QZLin <qzlin01@163.com>
2022-02-28 09:18:39 +0800

    resize image in preprocess

--- tool/avg_bg_handle.py
+++ tool/avg_bg_handle.py
@@ -8,17 +8,18 @@ import cvtool
 
 parser = argparse.ArgumentParser()
 parser.add_argument('image')
-parser.add_argument('-f', '--file', action='store_true')
-parser.add_argument('-o', '--output', type=str)
+parser.add_argument('-f', '--file', type=str)
+# parser.add_argument('-o', '--output', type=str)
 args = parser.parse_args()
 # print(args.image)
 
 img = cvtool.im.sim_screen_crop((1280, 720), cvtool.im.uimread(args.image))
+img = cv.resize(img, (1280, 720), interpolation=cv.INTER_CUBIC)  # cv.INTER_LINEAR
 # cvtool.im.test_img(img)
 out = cv.imencode('.png', img)[1]
 
 if args.file:
-    cvtool.im.uimwrite(args.output, '.png', img)
+    cvtool.im.uimwrite(args.file, '.png', img)
 else:
     with fdopen(stdout.fileno(), "wb", ) as stdout:
         stdout.write(out)
