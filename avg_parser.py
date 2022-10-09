d7824b111bd4ed003a538ae8097e3d9cb01f8378
QZLin <qzlin01@163.com>
2022-10-09 22:45:24 +0800

    split method

--- avg_parser.py
+++ avg_parser.py
@@ -56,71 +56,9 @@ def parser(source, label=None):
     ast_map = [ast.Statement(['stop', 'sound'])]
     ast_top = []
     for line in lines:
-        # NPC-Kalin(1)<Speaker>格琳</Speaker>||:“选择我们，加入我们！格里芬私人军事承包商，更新世界的锋芒！”+没错，从今天开始，您就是格里芬旗下的战术指挥官啦！
-        name = None
+        speakers, tags = parse_tags(ast_top, ast_map, cname_name, name_cname, line)
 
-        head = line[:line.find(':')]
         text = line[line.find(':') + 1:]
-        tag_head = head[head.find('||') + 2:]
-        # Parse All tag by regex match
-        full_tags = re.finditer('<(\\S+?)>(.+?)</\\1>', head)
-        half_head = re.sub('<(\\S+?)>(.+?)</\\1>', "", head)
-        half_tags = re.finditer('<(\\S+?)>', half_head)
-
-        tags = [{'key': x.groups()[0], 'value': x.groups()[1], 'close': True} for x in full_tags]
-        tags.extend([{'key': x.groups()[0], 'value': None, 'close': False} for x in half_tags])
-
-        speakers = []
-        for tag in tags:
-            match tag['key']:
-                case 'BGM':
-                    bgm = patch.bgm_patcher(tag['value'])
-                    if (path := bgmr.pl.fpath(bgm + '.wav')) is None:
-                        path = 'musiclost.flac'
-                        s_apd(f'LOST: {bgm}', debug_bgm)
-                    else:
-                        path = path.replace('\\', '/')
-                        s_apd(path, debug_bgm)
-                    ast_map.append(ast.Statement(['play', 'music'], ast.Str(path)))
-                case 'SE1' | 'SE2':
-                    sound_fx = tag['value']
-                    s_apd(sound_fx, debug_sound_fx)
-                    path = f'audio/{sound_fx}.wav'
-                    ast_map.append(ast.Statement(['play', 'sound'], ast.Str(path)))
-                case 'BIN':
-                    bg_result = tag['value']
-                    bg_img = IMG[int(bg_result.replace(' ', ''))]
-                    bg_code_name = f'i_{to_codename(bg_img)}'
-
-                    ast_map.append(ast.Assign(f'i_{to_codename(bg_img)}', 'image', f'images/{bg_img}.png'))
-                    ast_map.append(ast.Statement(['scene'], bg_code_name))
-                case 'Speaker':
-                    name = tag['value']
-                    if name is None or name.isspace():
-                        break
-                    code_name = to_codename(name)
-                    if name not in name_cname.keys():
-                        name_cname[name] = code_name
-                        cname_name[code_name] = name
-                        s_apd(name, debug_names)
-                        ast_top.append(ast.Assign(code_name, 'define', ast.Func('Character', name)))
-                    speakers.append(code_name)
-                # TODO
-                case '通讯框':
-                    pass
-                case 'Grey':
-                    pass
-                case 'Shake':
-                    pass
-                case 'Position':
-                    pass
-                case '回忆':
-                    pass
-                case '关闭蒙版':
-                    pass
-                case '名单' | '名单2':
-                    pass
-
         # TODO Character Portrait
 
         # Convert color tag
@@ -146,7 +84,65 @@ def parser(source, label=None):
     ast_top.append(ast.Block('label', label, ast_map))
     result = ast.ast2rpy(ast_top)
     return result
-    # return char_define(names) + '\n' + add_indentation(avg_text, label=label)
+
+
+# NPC-Kalin(1)<Speaker>格琳</Speaker>||:“选择我们，加入我们！格里芬私人军事承包商，更新世界的锋芒！”+没错，从今天开始，您就是格里芬旗下的战术指挥官啦！
+def parse_tags(ast_top, ast_map, cname_name, name_cname, line):
+    head = line[:line.find(':')]
+    head2 = re.sub('<(\\S+?)>(.+?)</\\1>', '', head)
+
+    # Parse All tag by regex match
+    full_tags = re.finditer('<(\\S+?)>(.+?)</\\1>', head)
+    half_tags = re.finditer('<(\\S+?)>', head2)
+
+    tags = [{'key': x.groups()[0], 'value': x.groups()[1], 'close': True} for x in full_tags]
+    tags.extend([{'key': x.groups()[0], 'value': None, 'close': False} for x in half_tags])
+
+    speakers = []
+    for tag in tags:
+        match tag['key']:
+            case 'BGM':
+                path = bgmr.get_bgm(tag['value'])
+                ast_map.append(ast.Statement(['play', 'music'], ast.Str(path)))
+            case 'SE1' | 'SE2':
+                sound_fx = tag['value']
+                s_apd(sound_fx, debug_sound_fx)
+                path = f'audio/{sound_fx}.wav'
+                ast_map.append(ast.Statement(['play', 'sound'], ast.Str(path)))
+            case 'BIN':
+                bg_result = tag['value']
+                bg_img = IMG[int(bg_result.strip(' '))]
+                bg_code_name = f'i_{to_codename(bg_img)}'
+
+                ast_map.append(ast.Assign(f'i_{to_codename(bg_img)}', 'image', f'images/{bg_img}.png'))
+                ast_map.append(ast.Statement(['scene'], bg_code_name))
+            case 'Speaker':
+                name = tag['value']
+                if name is None or name.isspace():
+                    break
+                code_name = to_codename(name)
+                if name not in name_cname.keys():
+                    name_cname[name] = code_name
+                    cname_name[code_name] = name
+                    s_apd(name, debug_names)
+                    ast_top.append(ast.Assign(code_name, 'define', ast.Func('Character', name)))
+                speakers.append(code_name)
+            # TODO
+            case '通讯框':
+                pass
+            case 'Grey':
+                pass
+            case 'Shake':
+                pass
+            case 'Position':
+                pass
+            case '回忆':
+                pass
+            case '关闭蒙版':
+                pass
+            case '名单' | '名单2':
+                pass
+    return speakers, tags
 
 
 if __name__ == '__main__':
--- tool/bgm_path_resolver.py
+++ tool/bgm_path_resolver.py
@@ -1,11 +1,26 @@
 import cvtool
 
+import patch
 import tool.config as config
 
 pl = cvtool.pla.PLAsset(config.RENPY_PROJECT, rel_root=config.RENPY_GAME, path_sep='/')
 pl.build_library()
 
 UI_ObjDown = pl.fpath('UI_ObjDown.wav')
+
+
+def get_bgm(key):
+    bgm = patch.bgm_patcher(key)
+    if (path := pl.fpath(bgm + '.wav')) is None:
+        path = 'musiclost.flac'
+        with open('debug/bgm.txt', 'a') as f:
+            f.write('LOST: {bgm}\n')
+    else:
+        path = path.replace('\\', '/')
+        with open('debug/bgm.txt', 'a') as f:
+            f.write(' {bgm}\n')
+    return path
+
 
 if __name__ == "__main__":
     print(pl.library)
